42497f47f46b0c046c14be359471d5d529fef019
Mongkhon handsome and cool. <79182778+Mongkol30@users.noreply.github.com>
2026-05-02 16:47:54 +0700

    feat: add CORS configuration

---
+++ src/main/java/com/bym/beeryyummymap/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.bym.beeryyummymap.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("https://bym-front.netlify.app");
+        config.addAllowedOrigin("http://localhost:4200"); // สำหรับ dev
+
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
